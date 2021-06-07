0c92aad9c6e69d5a813418665cff53a0f87bf184
Shehab1Noaman <78321126+Shehab1Noaman@users.noreply.github.com>
2021-06-07 14:35:49 +0100

    Add files via upload

---
+++ Script.ps1
@@ -0,0 +1,7 @@
+﻿
+
+$Aduser = Get-ADUser -Filter {-not(Title -like "*") -or -not(Department -like "*") -or -not(Company -like "*") } -Properties Name,Surname,GivenName,mail,Title,Department,Manager,Company 
+
+$Adrepot = $Aduser | Where { $_.Enabled -eq $True} | select Name,Surname,GivenName,mail,Title,Department,Company,Manager,Status 
+
+$Adrepot | sort Name | Export-Csv c:\temp\UsersReport.csv -NoTypeInformation
